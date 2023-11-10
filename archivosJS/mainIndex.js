8bbd6106592f546c3476ded30d51695c9931c633
Jose Imbrenda <joseimbrenda@MacBook-Air-de-Jose.local>
2023-11-10 16:53:26 -0300

    se agregan JSON de ejercicios

--- archivosJS/mainIndex.js
+++ archivosJS/mainIndex.js
@@ -1,19 +1,22 @@
 if (recuperarUsuarioLogueado()) {
   const NODO_NAV = document.getElementById("ul");
   
-  crearNodo(NODO_NAV, "li", "<a>Mi Rutina</a>").children[0].setAttribute(
+  crearNodo(NODO_NAV, "li", "<a>Rutina</a>").children[0].setAttribute(
     "href",
     "./pages/miRutina.html"
   );
   crearNodo(
     NODO_NAV,
     "li",
-    "<a>Mi Plan de Alimentacion</a>"
+    "<a>Alimentacion</a>"
   ).children[0].setAttribute("href", "./pages/miPlanDeAlimentacion.html");
   crearNodo(NODO_NAV, "button", "Cerrar Sesion").setAttribute(
     "id",
     "cerrarSesion"
   );
+  const aCreados =document.querySelectorAll('nav ul li a')
+  aCreados.forEach((a) => a.className= 'btn btn-primary btn-block btn-large')
+
   const REMOVER = document.getElementsByClassName('remover')
   for (let i = 0; i < 2; i++) {NODO_NAV.removeChild(REMOVER[0])}
 
